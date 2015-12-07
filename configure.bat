618c86170e6409db44df2aa23263faef761df991
pombredanne <pombredanne@nexb.com>
2015-12-07 13:43:31 +0100

    Revert "Removed unused option for --init"
    This reverts commit d6dc482ca02dfe32f59781dd9f3ea64a79672e8a.

--- configure.bat
+++ configure.bat
@@ -34,6 +34,11 @@ if "%SCANCODE_CLI_ARGS%"==" " (
     goto configure
 )
 
+if "%SCANCODE_CLI_ARGS%"=="  --init" (
+    set SCANCODE_CLI_ARGS="%CONF_INIT%"
+    goto configure
+)
+
 :configure
 if not exist "c:\python27\python.exe" (
     echo(
